dc878eb9993e4866e3460e017b4e4b76cf7e1117
Jordi Baylina <jordi@baylina.cat>
2017-10-31 08:54:15 -0500

    Import from the minime branch

---
+++ contracts/IERC223bMiniMeTokenController.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+/// @dev The token controller contract must implement these functions
+interface IERC223bMiniMeTokenController {
+    /// @notice Called when `_owner` sends ether to the MiniMe Token contract
+    /// @param _owner The address that sent the ether to create tokens
+    /// @return True if the ether is accepted, false if it throws
+    function proxyPayment(address _owner) public payable returns(bool);
+
+    /// @notice Notifies the controller about a token transfer allowing the
+    ///  controller to react if desired
+    /// @param _from The origin of the transfer
+    /// @param _to The destination of the transfer
+    /// @param _amount The amount of the transfer
+    /// @return False if the controller does not authorize the transfer
+    function onTransfer(address _from, address _to, uint _amount, bytes data) public returns(bool);
+}
